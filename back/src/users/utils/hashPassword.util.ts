22b41d6bfd00e06496a4e1ddf386869de7ae8250
Aleksandr Borovsky <kycok.6o6pa@gmail.com>
2023-02-14 23:43:40 +0100

    Minor hashPassword fix

--- back/src/users/utils/hashPassword.util.ts
+++ back/src/users/utils/hashPassword.util.ts
@@ -2,11 +2,5 @@ import * as bcrypt from 'bcrypt';
 
 const BCRYPT_SALT_ROUNDS = 12;
 
-export const hashPassword = async (password: string): Promise<string> => {
-  try {
-    return await bcrypt.hash(password, BCRYPT_SALT_ROUNDS);
-  } catch (e) {
-    debugger;
-    throw e;
-  }
-};
+export const hashPassword = (password: string): Promise<string> =>
+  bcrypt.hash(password, BCRYPT_SALT_ROUNDS);
